18d7e2d9b79f534a69e858d9891c98f30d78e33f
VentGrey <omarpurataf@gmail.com>
2020-09-08 21:51:13 -0500

    Finished adding the quickopts

--- wpm.sh
+++ wpm.sh
@@ -16,11 +16,36 @@ help() {
 
     options:
 
-    --help | -h : Show this screen
-    --sysup
+    --help    | -h : Show this screen
+    --version | -v: prints version
+    --contrib | -c: lists contributors
+    --request | -r: Opens a browser to request a new feature
+
+    quickopts:
+       upd:          Updtates the system.
+       auto:         Enables automatic updates.
+       auto-sec:     Enables automatic security updates only.
+       dis-root:     Disables root account.
+       addswp:       Adds swap to the server.
+       sysconf:      Various sysctl.conf tweaks
+       dis-irq:      Disables IRQ Balance.
+       heartbleed:   Checks for heartbleed CVE in OpenSSL.
+       sec-tmp:      Secures /tmp and /var/tmp.
+       sec-shared:   Secures shared memory.
+       ch-hname:     Change server hostname.
+       bomb-shield:  Sets proccess limit for users (protects against fork-bombs)
+       spoof-shield: Prevents IP Spoofing.
+       ddos-shield:  Protects against DDoS attacks.
+       harden-php:   Hardens php.ini file.
+       harden-ssh:   Hardens sshd_config file.
+       ipt-rules:    Various iptables rules that MIGHT be handy.
+       shellshock:   Check if your current bash version is compromised.
 
 __EOF__
+
+exit 1
 }
+
 
 
 # Weeping Moon - UpVent 2020
