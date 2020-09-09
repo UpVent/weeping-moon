3ef13c9a0802af102da48a6999c1f1926e638211
VentGrey <omarpurataf@gmail.com>
2020-09-08 20:44:19 -0500

    Minor changes

---
+++ wpm.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+help() {
+	cat << __EOF__
+
+    Weeping Moon
+
+    usage: wpm [options] [quickopts]
+
+    [options] - weeping moon options. Mostly to help the user on weeping moon
+    script usage.
+
+    [quickopts] - Quick options for weeping moon that execute a small bundle of
+    functions. Ideal if you want something that "just works" without having to
+    prompt anything.
+
+    options:
+
+    --help | -h : Show this screen
+    --sysup
+
+__EOF__
+}
+
+
+# Weeping Moon - UpVent 2020
